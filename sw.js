cb86d5efb7e7d97e14363074c890a2fe8057061d
paulo19931990-gif <paulo19931990@gmail.com>
2026-08-22 22:50:07 -0300

    Increment CACHE_NAME to v11
    Updated CACHE_NAME to v11 to force cache refresh.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'multios-pro-v10'; // Aumentei para v10 para forçar nova atualização
+const CACHE_NAME = 'multios-pro-v11'; // Aumentei para v11 para forçar nova atualização
 const urlsToCache = [
   './', // Cobre o acesso ao domínio sem o index.html explícito
   './index.html',
